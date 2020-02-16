6fb5a422f6dcdd4f5bf784db1e4ceaf46180e32a
謝佳峻 <scott@mac.local>
2020-02-17 00:32:53 +0800

    refine calendar style

--- src/components/weeks.js
+++ src/components/weeks.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './myDatePicker.scss';
 import { weekHeaders } from './utils/util';
 import classNames from 'classnames';
 import SelectedTableBody from './selectedTableBody';
+import moment from 'moment';
 
 export default function Weeks(props) {
   const { weeks, current, year, month, day, onSelect } = props;
+  const now = useMemo(() => moment().format('YYYY-M-DD'), []);
   const renderWeeksHeader = () => {
     return weekHeaders.map((head) => {
       return (
@@ -29,8 +31,11 @@ export default function Weeks(props) {
           onSelect={onSelect}
           columnHeight={50}
           renderComponent={(week) => {
+            const date = `${week.year}-${week.month}-${week.day}`;
             const selected = week.year === year && week.month === month && week.day === day;
-            const fontColor = selected ? '#fff' : week.month === current.month? '#000' : '#A9A9A9';
+            const fontColor = selected
+              ? '#fff' : week.month === current.month 
+              ? (now === date ? '#db3d44' : '#000') : '#eeeeee';
             return (
               <div
                 style={{
